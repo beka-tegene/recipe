ca6a3e4b87a479081deecbc97c803ca90887fc7b
beka-tegen <bekategene177@gmail.com>
2023-12-04 11:15:15 +0300

    modify

--- src/Store/API/Recipe.js
+++ src/Store/API/Recipe.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const CreateRecipe = async (data) => {
+  console.log(data);
   const useData = await axios.post(
-    "http://localhost:5000/api/v1/recipe/",
+    "http://localhost:5000/api/v1/recipe/createRecipe",
     data,
     {
       headers: {
-        "Content-Type": "multipart/form-data",
+         "Content-Type": "multipart/form-data",
       },
     }
   );
   console.log(useData);
-  return useData;
+  if (useData.status === 200) {
+    window.location.reload(true);
+  }
 };
-
-
-
--- src/Store/Middleware/Middleware.js
+++ src/Store/Middleware/Middleware.js
@@ -2,10 +2,13 @@ import { call, takeLatest } from "redux-saga/effects";
 import { toast } from "react-toastify";
 import { Login, SignUp } from "../API/Auth";
 import { setLoginData, setRegisterData } from "../Hook/AuthHook";
+import { CreateRecipe } from "../API/Recipe";
+import { setCreateRecipeData } from "../Hook/RecipeHook";
 
 export function* watchRecipe() {
   yield takeLatest("auth/setRegister", fetchSetRegister);
   yield takeLatest("auth/setLogin", fetchSetLogin);
+  yield takeLatest("recipe/setCreateRecipe", fetchCreateRecipe);
 }
 
 // Authentication and Authorization data
@@ -23,6 +26,15 @@ function* fetchSetLogin(action) {
   try {
     yield call(Login, action.payload.data);
     yield setLoginData();
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+}
+
+function* fetchCreateRecipe(action) {
+  try {
+    yield call(CreateRecipe, action.payload);
+    yield setCreateRecipeData();
   } catch (error) {
     toast.error(error.response.data.msg);
   }
