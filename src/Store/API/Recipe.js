f0c84a5db726318975e49d9ee66f8c6956e2256c
chapi4443 <yamyabu35i@gmail.com>
2023-12-04 11:15:18 +0300

    modify

--- src/Store/API/Recipe.js
+++ src/Store/API/Recipe.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 export const CreateRecipe = async (data) => {
   const useData = await axios.post(
     "http://localhost:5000/api/v1/recipe/",
@@ -14,5 +15,60 @@ export const CreateRecipe = async (data) => {
   return useData;
 };
 
+export const getAllRecipes = async (data) => {
+  //
+  const response = await axios.get(
+    `http://localhost:5000/api/v1/recipe`
+    );
 
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw new Error("Failed to fetch recipes");
+  }
+};
+
+
+
+export const getRecipeById = async (data) => {
+  //
+  const response = await axios.get(
+    `http://localhost:5000/api/v1/recipe/${data}`
+  );
+
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw new Error("Failed to fetch recipes by id");
+  }
+};
+
+
+
+export const updateRecipeById= async (data) => {
+  //
+  const response = await axios.get(
+    `http://localhost:5000/api/v1/recipe/${data}`
+  );
+
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw new Error("Failed to fetch recipes by id");
+  }
+};
+
+
+export const deleteRecipeById = async (data) => {
+  //
+  const response = await axios.get(
+    `http://localhost:5000/api/v1/recipe/${data}`
+  );
+
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    throw new Error("Failed to fetch recipes by id");
+  }
+};
 
